b1ec4f78d9bdaa84bd500b26a2832e96430c3224
Liban Jama <jliban756@gmail.com>
2026-04-14 11:04:52 +0100

    completed level 100

--- script.js
+++ script.js
@@ -6,7 +6,27 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const mainElem = document.createElement("main");
+  mainElem.classList.add("container");
+
+  episodeList.forEach((episode) => {
+    const { name, season, number, image, summary } = episode;
+    const paddedSeason = String(season).padStart(2, "0");
+    const paddedNumber = String(number).padStart(2, "0");
+
+    const episodeDiv = document.createElement("div");
+    episodeDiv.classList.add("episode-card");
+    episodeDiv.innerHTML = `
+    <h2 class="title">${name} - S${paddedSeason}E${paddedNumber}</h2>
+    <img
+  class="episode-image"
+  src="${image.medium}"
+  alt="episode image" />
+  ${summary} 
+  `;
+    mainElem.appendChild(episodeDiv);
+    rootElem.appendChild(mainElem);
+  });
 }
 
 window.onload = setup;
